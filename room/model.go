b2b8d1035c1483bf4fc4df2efbcf0debb76d000e
Luis <lalvarezguillen@gmail.com>
2017-11-21 20:34:23 +0100

    extended models

--- room/model.go
+++ room/model.go
@@ -6,6 +6,38 @@ import (
 	"github.com/lalvarezguillen/roomies/person"
 )
 
+// Rules represents the convivence rules set by the landlord.
+type Rules struct {
+	PetsOK     bool
+	SmokingOK  bool
+	VisitorsOK bool
+}
+
+// Address holds the address of a room
+type Address struct {
+	Country    string
+	Region     string
+	City       string
+	Street     string
+	Number     string
+	Apartament string
+}
+
+// Coordinates holds a latitude-longitude pair
+type Coordinates struct {
+	Lat  float32
+	Long float32
+}
+
+// MediaFile represents images and videos that can be associated to rooms
+// or users
+type MediaFile struct {
+	ID           string
+	MimeType     string
+	ThumbnailURL string
+	URL          string
+}
+
 // Room represents a room on the market
 type Room struct {
 	ID               string
@@ -13,10 +45,12 @@ type Room struct {
 	Description      string
 	RoommatesCount   int
 	Roomates         []person.Person
-	PetsOK           bool
-	SmokingOK        bool
 	RegistrationDate time.Time
 	Price            float32
+	Rules            Rules
+	Address          Address
+	Location         Coordinates
+	Media            []MediaFile
 }
 
 // Rooms represents a list of rooms
